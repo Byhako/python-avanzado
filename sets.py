8d8a8f7c421d5edfc7b89238162f64d34df5f6e0
Ruben E Acosta <reacosta@grupo-exito.com>
2022-02-24 16:13:25 -0500

    sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+c1 = {1, 2, 3, 4}
+c2 = {4, 3, 5 ,6}
+
+# Union
+print('Union: ', c1 | c2)
+
+# Interseccion
+print('Interseccion', c1 & c2)
+
+# Diferencia
+print('Diferencia', c1 - c2)
+
+# Diferencia simetrica, opuesto a interseccion
+print('Diferencia simetrica', c1 ^ c2)
