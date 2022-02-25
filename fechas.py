efc54a733d258e25c550ed5ea08ae7f43964d057
Ruben E Acosta <eldragondera1@gmail.com>
2022-02-25 09:52:23 -0500

    zonas horarias

--- fechas.py
+++ fechas.py
@@ -9,3 +9,18 @@ my_str = my_time.strftime('%d/%m/%y')
 my_str = my_time.strftime('El ano es: %y')
 
 print(my_str)
+
+# Zonas horarias.
+
+import pytz
+
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+bogota = pytz.timezone('America/Bogota')
+bogota_date = datetime.datetime.now(bogota)
+print(f'Bogota: {bogota_date}')
+
+mexico = pytz.timezone('America/Mexico_City')
+mexico_date = datetime.datetime.now(mexico)
+print(f'Mexico: {mexico_date}')
+
